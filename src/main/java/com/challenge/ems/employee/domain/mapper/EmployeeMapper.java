a67a9078b08cde2a8e6de9d0583ed9be19a2e76a
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-07-22 15:13:37 +0200

    feat(employee): add  mapEmployeeListToJson at EmployeeMapper

--- src/main/java/com/challenge/ems/employee/domain/mapper/EmployeeMapper.java
+++ src/main/java/com/challenge/ems/employee/domain/mapper/EmployeeMapper.java
@@ -2,13 +2,19 @@ package com.challenge.ems.employee.domain.mapper;
 
 import com.challenge.ems.employee.domain.commands.EmployeeCommand;
 import com.challenge.ems.employee.domain.model.Employee;
+import com.challenge.ems.employee.presentation.json.EmployeeJson;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
 
+    EmployeeMapper INSTANCE = Mappers.getMapper( EmployeeMapper.class );
+
     Employee mapToModel(EmployeeCommand employeeCommand);
     void mapToModel(EmployeeCommand employeeCommand, @MappingTarget Employee employee);
+    List<EmployeeJson> mapEmployeeListToJson(List<Employee> employees);
 
 }
