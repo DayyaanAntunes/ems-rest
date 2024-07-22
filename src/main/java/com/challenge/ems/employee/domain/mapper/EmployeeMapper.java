31136654808b61d594516b05f1de3d002a73a530
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-07-22 09:11:51 +0200

    feat(employee): Add a Mapper for Employee

---
+++ src/main/java/com/challenge/ems/employee/domain/mapper/EmployeeMapper.java
@@ -0,0 +1,15 @@
+package com.challenge.ems.employee.domain.mapper;
+
+import com.challenge.ems.employee.domain.commands.EmployeeCommand;
+import com.challenge.ems.employee.domain.model.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+
+    Employee mapToModel(EmployeeCommand employeeCommand);
+    void mapToModel(EmployeeCommand employeeCommand, @MappingTarget Employee employee);
+
+}
