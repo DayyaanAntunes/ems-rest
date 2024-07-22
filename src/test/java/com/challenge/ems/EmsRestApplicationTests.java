d5d215e7764f91b6eb05178248d662f74c32db91
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-07-22 09:27:26 +0200

    chore(build): add automatically generated files

---
+++ src/test/java/com/challenge/ems/EmsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
