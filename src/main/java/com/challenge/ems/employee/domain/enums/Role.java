e988bdf7da7e23cd3eb89d42cbd1de05c5b2b68b
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-07-22 09:06:34 +0200

    feat(employee): Add Enum for Role field

---
+++ src/main/java/com/challenge/ems/employee/domain/enums/Role.java
@@ -0,0 +1,14 @@
+package com.challenge.ems.employee.domain.enums;
+
+public enum Role {
+    BACKEND_DEVELOPER,
+    FRONTEND_DEVELOPER,
+    FULLSTACK_DEVELOPER,
+    PROJECT_MANAGER,
+    ADMINISTRATOR,
+    CEO,
+    TECHNICAL_SUPPORT,
+    DEVOPS_ENGINEER,
+    UI_UX_DESIGNER,
+    STAFF
+}
