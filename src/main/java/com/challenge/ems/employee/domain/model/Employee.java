0089dd859c6424af1fcab7413c56c49bd96d5438
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-07-22 09:00:32 +0200

    feat(employee): Add Employee model class

---
+++ src/main/java/com/challenge/ems/employee/domain/model/Employee.java
@@ -0,0 +1,53 @@
+package com.challenge.ems.employee.domain.model;
+
+import com.challenge.ems.employee.domain.enums.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private LocalDate birthDate;
+    private String nationality;
+    private String address;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private Availability availability;
+    @Enumerated(EnumType.STRING)
+    private EmploymentStatus employmentStatus;
+    @Enumerated(EnumType.STRING)
+    private ModalityType modalityType;
+    private String idNumber;
+    private String nuit;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "employee_roles", joinColumns = @JoinColumn(name = "employee_id"))
+    @Column(name = "role")
+    private List<Role> roles;
+
+    private Double salary;
+    private String email;
+    private String phoneNumber;
+    private String socialSecurityNumber;
+    private LocalDate startedAt;
+    private LocalDate terminatedAt;
+    private Byte contractYears;
+}
