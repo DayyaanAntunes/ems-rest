6810389a77cd8f58673b99d1863bd9879b366ffa
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-08-04 23:05:36 +0200

    feat(employee): add unique constraint validation to EmployeeService

--- src/main/java/com/challenge/ems/employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/challenge/ems/employee/service/EmployeeServiceImpl.java
@@ -1,21 +1,20 @@
 package com.challenge.ems.employee.service;
 
+import com.challenge.ems.configuration.exception.ResponseException;
 import com.challenge.ems.employee.domain.commands.EmployeeCommand;
 import com.challenge.ems.employee.domain.mapper.EmployeeMapper;
 import com.challenge.ems.employee.domain.model.Employee;
 import com.challenge.ems.employee.persistence.EmployeeRepository;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+
     @Autowired
     private EmployeeRepository employeeRepository;
-
     @Autowired
     private EmployeeMapper employeeMapper;
 
@@ -24,19 +23,20 @@ public class EmployeeServiceImpl implements EmployeeService{
         try{
             return employeeRepository.findAll();
         }catch (Exception e){
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "internal");
+            throw new ResponseException("internal", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @Override
     public Employee findEmployeeById(Long id) {
             return employeeRepository.findById(id).orElseThrow(
-                    ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "employee.not-found"));
+                    ()-> new ResponseException("employee.not-found", HttpStatus.NOT_FOUND));
     }
 
     @Override
     public Employee save(EmployeeCommand employeeCommand) {
             Employee employee = employeeMapper.mapToModel(employeeCommand);
+            checkForUniqueConstraints(employee);
             return saveOrUpdateEmployee(employee);
     }
 
@@ -48,28 +48,41 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     }
 
+    private void checkForUniqueConstraints(Employee employee) {
+        if (employeeRepository.existsByEmail(employee.getEmail())) {
+            throw new ResponseException("employee.email-already-exists", HttpStatus.CONFLICT);
+        }
+        if (employeeRepository.existsByIdNumber(employee.getIdNumber())) {
+            throw new ResponseException("employee.id_number-already-exists", HttpStatus.CONFLICT);
+        }
+        if (employeeRepository.existsByNuit(employee.getNuit())) {
+            throw new ResponseException("employee.nuit-already-exists", HttpStatus.CONFLICT);
+        }
+        if (employeeRepository.existsBySocialSecurityNumber(employee.getSocialSecurityNumber())) {
+            throw new ResponseException("employee.social_security_number-already-exists", HttpStatus.CONFLICT);
+        }
+    }
+
     private Employee saveOrUpdateEmployee(Employee employee){
         try {
             return employeeRepository.save(employee);
-        } catch (ConstraintViolationException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "employee.email-already-exists");
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "internal");
+            throw new ResponseException("internal", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @Override
     public void delete(Long id) {
-        try{
-            if (employeeRepository.existsById(id)) {
-                employeeRepository.deleteById(id);
-            } else {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "employee.not-found");
+        try {
+            if (!employeeRepository.existsById(id)) {
+                throw new ResponseException("employee.not-found", HttpStatus.NOT_FOUND);
             }
+            employeeRepository.deleteById(id);
+        } catch  (ResponseException e){
+            throw e;
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "internal");
+            throw new ResponseException("internal", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
 
 }
