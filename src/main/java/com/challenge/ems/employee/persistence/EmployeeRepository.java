776c7d8a35be773987807dbf22a9bad80f29dadf
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-08-04 23:03:53 +0200

    feat(employee): add validation for fields with unique constraint to EmployeeRepository

--- src/main/java/com/challenge/ems/employee/persistence/EmployeeRepository.java
+++ src/main/java/com/challenge/ems/employee/persistence/EmployeeRepository.java
@@ -4,4 +4,8 @@ import com.challenge.ems.employee.domain.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByIdNumber(String idNumber);
+    boolean existsByNuit(String nuit);
+    boolean existsBySocialSecurityNumber(String socialSecurityNumber);
 }
